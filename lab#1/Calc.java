4e6d03294b1aebf5329b2e5bd20f9e72c49c79d8
MaximShuvalov <shuvalov99.me@gmail.com>
2020-09-15 09:52:34 +0600

    Corrected comments

--- lab#1/Calc.java
+++ lab#1/Calc.java
@@ -1,28 +1,31 @@
 public class Calc {
 
     public static void main(String[] args) {
-        for (String str : args) {
-            System.out.println("аргумент = " + str);
-        }
-
-        System.out.println("Ответ = " + Calculate(args[0], args[1], args[2]));
+        if (args.length == 3) {
+            for (String str : args) {
+                System.out.println("аргумент = " + str);
+            }
+            System.out.println("Ответ = " + Calculate(args[0], args[1], args[2]));
+        } else
+            System.out.println("Должно быть 3 входных параметра!");
     }
 
     public static double Calculate(String a, String b, String c) {
+        double eq = 1e-12;
         double aD = Double.parseDouble(a);
         double cD = Double.parseDouble(c);
-        switch (b) {
-            case "+": {
+        switch (b.charAt(0)) {
+            case '+': {
                 return aD + cD;
             }
-            case "-": {
+            case '-': {
                 return aD - cD;
             }
-            case "*": {
+            case '*': {
                 return aD * cD;
             }
-            case "/": {
-                if(aD == 0 || cD == 0)
+            case '/': {
+                if (cD < eq)
                     throw new IllegalArgumentException("Делить на 0 нельзя!!");
                 return aD / cD;
             }
