5c5cfa8394626709186daec8ef5b4f617dca1eb3
mshuvalov <shuvalov99.me@gmail.com>
2020-09-30 00:12:13 +0600

    * Добавил описание причины присвоения ачивки.

--- lab#4/Achievement.java
+++ lab#4/Achievement.java
@@ -1,29 +1,36 @@
 public class Achievement {
     private Achievements achievement;
     private String name;
+    private String note = "Описание отсутствует";
 
-    Achievement(Achievements ach){achievement = ach;}
+    Achievement(Achievements ach) {
+        achievement = ach;
+    }
 
-    public String GetName(){
+    public String GetName() {
         GetRusName();
         return name;
     }
 
-    private void GetRusName(){
-        switch(achievement){
+    public void SetNote(String note) { this.note = note;}
+
+    public String GetNote() { return note;}
+
+    private void GetRusName() {
+        switch (achievement) {
             case BestProgrammerOfGroup:
-            name = "Лучший программист в группе";
-            break;
+                name = "Лучший программист в группе";
+                break;
             case WorseProgrammerOfCourse:
-            name = "Худший программист в группе";
-            break;
-        default:
-            break;
+                name = "Худший программист в группе";
+                break;
+            default:
+                break;
         }
     }
 
     @Override
-    public String ToString(){
-        return "Ачивка с названием - " + name;
+    public String ToString() {
+        return "Ачивка с названием - " + name + " и описанием - " + note;
     }
 }
