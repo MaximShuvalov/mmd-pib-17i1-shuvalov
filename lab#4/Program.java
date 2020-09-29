401d345af0f4c85dc83e2ffd3221f1cd265d0046
mshuvalov <shuvalov99.me@gmail.com>
2020-09-30 00:33:15 +0600

    * Создание экземпляра достижения.

--- lab#4/Program.java
+++ lab#4/Program.java
@@ -1,5 +1,9 @@
 public class Program {
     public static void main(String[] args) {
-        
+        Achievement a1 = new Achievement(Achievements.BestProgrammerOfGroup);
+
+        System.out.println("Название ачивки - " + a1.GetName());
+
+        System.out.println(a1);
     }
 }
