07460ab446571668648988d6d9fd6fc6b9e44f72
Шувалов Максим Евгеньевич <mshuvalov@gems.local>
2020-11-01 15:47:13 +0600

    + Сущность учебной группы.

---
+++ lab#4/StudyGroup.java
@@ -0,0 +1,35 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class StudyGroup {
+    private List<Student> _students = new ArrayList<Student>();
+    private String _name;
+
+    public StudyGroup(String name) {
+        _name = name;
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+    public void addStudents(Student student) {
+        if (student.equals(null))
+            return;
+        _students.add(student);
+    }
+
+    public Student getStudent(String nameStudent) {
+        return _students.stream().filter((student) -> student.getName() == nameStudent).findFirst()
+                .orElseGet(() -> null);
+    }
+
+    public List<Student> getStudents() {
+        return _students;
+    }
+
+    @Override
+    public String toString() {
+        return "Группа " + _name + " с количеством студентов " + _students.size();
+    }
+}
