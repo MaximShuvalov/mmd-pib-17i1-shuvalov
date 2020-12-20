8edac0c7a780ce44f5458a86b42627e5ce15a7fb
Шувалов Максим Евгеньевич <mshuvalov@gems.local>
2020-12-20 19:39:54 +0600

    + Обработчик для питомника. Метод добавления дня кормления.

---
+++ Coursework/NurseryHandler.java
@@ -0,0 +1,8 @@
+import ObjectModel.Animal;
+
+public class NurseryHandler {
+
+    public void feedAnimal(Animal animal, int countPackFood){
+        animal.feedAnimal(countPackFood);
+    }
+}
--- Coursework/ObjectModel/Animal.java
+++ Coursework/ObjectModel/Animal.java
@@ -1,5 +1,9 @@
 package ObjectModel;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Animal {
     public TypesAnimal Type;
@@ -8,9 +12,20 @@ public class Animal {
 
     public ArrayList<Day> Days;
 
-    public Animal(String name, TypesAnimal type, ArrayList<Day> days){
+    public Animal(String name, TypesAnimal type, ArrayList<Day> days) {
         Type = type;
         Name = name;
         Days = days;
+    }
+
+    public void feedAnimal(int countPackFood) {
+        var date = getCurrentDateString();
+        Days.add(new Day(date, countPackFood));
+    }
+
+    private String getCurrentDateString() {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        return dateFormat.format(date);
     }
 }
